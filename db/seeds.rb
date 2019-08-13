c0a3064e391811d043cd28a82676c6110710b4fb
learn <learn@learn16.local>
2019-08-13 13:57:22 -0700

    added_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,14 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+  # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+  # Character.create(name: 'Luke', movie: movies.first)
+
+  # User.create(one role for the manger and role for the bartender) rolls are assigned in rolify. Is it possible for this to store the info?
+
+  # make seed: user.create with EMAIL
+ manager = User.create(email:'manager@glasstrack.com', password: 'manager')
+
+
+ # assign to a variable and then do user.add role and that will give them the correct role.
